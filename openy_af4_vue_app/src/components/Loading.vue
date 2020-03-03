ee047a6171e10d714ab8bd6fe19e180ec43c1386
Vasyl Manachynskyi <v.manachynskyi@gmail.com>
2020-03-03 17:05:44 +0200

    [MPR-122] Implement loading indicator as in openY.

--- openy_af4_vue_app/src/components/Loading.vue
+++ openy_af4_vue_app/src/components/Loading.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="loading-component text-center">
-    <strong>LOADING</strong>
+    <svg class="spinner" viewBox="0 0 50 50" data-size="normal" data-flow="centered">
+      <circle
+        class="path"
+        cx="25"
+        cy="25"
+        r="20"
+        fill="none"
+        stroke-width="5"
+        stroke="#93bfec"
+      ></circle>
+    </svg>
   </div>
 </template>
 
@@ -9,3 +19,9 @@ export default {
   name: 'Loading'
 }
 </script>
+
+<style lang="scss">
+.loading-component {
+  height: 200px;
+}
+</style>
